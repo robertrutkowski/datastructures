f135e2ced1d44930e2828636468292456c29cc61
Robert Rutkowski <robert.rutkowski14@gmail.com>
2018-11-05 09:02:00 +0100

    List 80%

---
+++ DataStructures/DataStructures.Tests/List/ListTests.cs
@@ -0,0 +1,208 @@
+﻿using DataStructures.List;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Tests.List
+{
+    [TestClass]
+    public class ListTests
+    {
+        [TestMethod]
+        public void IndexerTest()
+        {
+            List<int> list = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            Assert.ThrowsException<IndexOutOfRangeException>(() => list[10]);
+            Assert.ThrowsException<IndexOutOfRangeException>(() => list[-1]);
+
+            Assert.IsTrue(list[0] == 0);
+            Assert.IsTrue(list[9] == 9);
+        }
+
+        [TestMethod]
+        public void ForeachTest()
+        {
+            List<int> list = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            int index = 0;
+            foreach (int i in list)
+            {
+                Assert.IsTrue(i == index++);
+            }
+            Assert.IsTrue(index == 10);
+        }
+
+        [TestMethod]
+        public void CountTest()
+        {
+            List<int> list = new List<int>();
+            Assert.IsTrue(list.Count == 0);
+
+            list = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Assert.IsTrue(list.Count == 10);
+        }
+
+        [TestMethod]
+        public void CapacityTest()
+        {
+            List<int> list = new List<int>();
+            Assert.IsTrue(list.Capacity == 0);
+
+            list = new List<int>(10);
+            Assert.IsTrue(list.Capacity == 10);
+
+            list = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Assert.IsTrue(list.Capacity == 16);
+        }
+
+        [TestMethod]
+        public void TrimExcessTest()
+        {
+            List<int> list = new List<int>();
+            list.TrimExcess();
+            Assert.IsTrue(list.Capacity == 0);
+
+            list = new List<int>(10);
+            list.TrimExcess();
+            Assert.IsTrue(list.Capacity == 0);
+
+            list = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            list.TrimExcess();
+            Assert.IsTrue(list.Capacity == 10);
+        }
+
+        [TestMethod]
+        public void ClearTest()
+        {
+            List<int> list = new List<int>();
+            list.Clear();
+            Assert.IsTrue(list.Count == 0);
+
+            list = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            list.Clear();
+            Assert.IsTrue(list.Count == 0);
+
+            Assert.ThrowsException<IndexOutOfRangeException>(() => list[0]);
+        }
+
+        [TestMethod]
+        public void InsertTests()
+        {
+            List<int> list = new List<int>() { 0, 2, 3, 4 };
+            Assert.ThrowsException<IndexOutOfRangeException>(() => list.Insert(5, 5));
+            Assert.ThrowsException<IndexOutOfRangeException>(() => list.Insert(-1, -1));
+
+            list.Insert(1, 1);
+            this.TestListElements(list);
+
+            list = new List<int>() { 1, 2, 3 };
+            list.Insert(0, 0);
+            this.TestListElements(list);
+        }
+
+        [TestMethod]
+        public void InsertRangeTests()
+        {
+            List<int> list = new List<int>() { 0, 2, 3, 4 };
+            Assert.ThrowsException<IndexOutOfRangeException>(() => list.InsertRange(5, new int[] { 5 }));
+            Assert.ThrowsException<IndexOutOfRangeException>(() => list.InsertRange(-1, new int[] { -1 }));
+
+            list.InsertRange(1, new int[] { 1 });
+            this.TestListElements(list);
+
+            list = new List<int>() { 0, 10, 11, 12, 13 };
+            list.InsertRange(1, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+            this.TestListElements(list);
+        }
+
+        [TestMethod]
+        public void AddRangeTests()
+        {
+            List<int> list = new List<int>() { 0, 1 };
+            list.AddRange(new int[] { 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+            this.TestListElements(list);
+        }
+
+        [TestMethod]
+        public void GetRangeTests()
+        {
+            List<int> list = new List<int>() { 0, 1 };
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => list.GetRange(5, 1));
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => list.GetRange(-1, 1));
+            Assert.ThrowsException<ArgumentException>(() => list.GetRange(1, -1));
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => list.GetRange(1, 6));
+
+            list = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            this.TestListElements(list.GetRange(0, 0));
+            this.TestListElements(list.GetRange(0, 10));
+            list = new List<int>() { 1, 1, 1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            this.TestListElements(list.GetRange(3, 8));
+        }
+
+        [TestMethod]
+        public void RemoveAtTests()
+        {
+            List<int> list = new List<int>() { 0, 1 };
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => list.RemoveAt(5));
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => list.RemoveAt(-1));
+
+            list = new List<int>() { 0, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            list.RemoveAt(1);
+            Assert.IsTrue(list.Count == 11);
+            this.TestListElements(list);
+
+            list = new List<int>() { 0 };
+            list.RemoveAt(0);
+            Assert.IsTrue(list.Count == 0);
+        }
+
+        [TestMethod]
+        public void RemoveRangeTests()
+        {
+            List<int> list = new List<int>() { 0, 1 };
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => list.GetRange(5, 1));
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => list.GetRange(-1, 1));
+            Assert.ThrowsException<ArgumentException>(() => list.GetRange(1, -1));
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => list.GetRange(1, 6));
+
+            list = new List<int>() { 0, 1, 1, 1, 1, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            list.RemoveRange(1, 5);
+            Assert.IsTrue(list.Count == 11);
+            this.TestListElements(list);
+
+            list = new List<int>() { 1 };
+            list.RemoveRange(0, 1);
+            Assert.IsTrue(list.Count == 0);
+        }
+
+        [TestMethod]
+        public void RemoveTests()
+        {
+            List<int> list = list = new List<int>() { 0, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Assert.IsTrue(list.Remove(1));
+            this.TestListElements(list);
+            Assert.IsFalse(list.Remove(100));
+        }
+
+        [TestMethod]
+        public void RemoveAllTests()
+        {
+            List<int> list = list = new List<int>() { 43, 564, 65, 87, 0, 1, 2, 3, 4, 5, 45, 54, 54, 65, 6, 7, 8, 9, 10 };
+            list.RemoveAll(x => x > 30);
+            this.TestListElements(list);
+        }
+
+        private void TestListElements(List<int> list)
+        {
+            int index = 0;
+            foreach (int i in list)
+            {
+                Assert.IsTrue(list[index] == index);
+                index++;
+            }
+        }
+    }
+}
---
+++ DataStructures/DataStructures/List/List.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataStructures.List
+{
+    public class List<T> : ICollection<T>
+    {
+        private T[] array;
+
+        public List(int capacity = 0)
+        {
+            this.array = new T[capacity];
+        }
+
+        public int Count { get; private set; }
+
+        public int Capacity
+        {
+            get
+            {
+                return this.array.Length;
+            }
+        }
+
+        public bool IsReadOnly => false;
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= this.Count)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+
+                return array[index];
+            }
+        }
+
+        public void Add(T item)
+        {
+            this.EnsureCapacity();
+            array[this.Count++] = item;
+        }
+
+        public void AddRange(IEnumerable<T> items)
+        {
+            foreach (T item in items)
+            {
+                this.Add(item);
+            }
+        }
+
+        public void TrimExcess()
+        {
+            Array.Resize(ref this.array, this.Count);
+        }
+
+        public void Clear()
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                this.array[i] = default(T);
+            }
+            this.Count = 0;
+        }
+
+        public void Insert(int index, T item)
+        {
+            if (index < 0 || index > this.Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            this.EnsureCapacity();
+
+            Array.Copy(this.array, index, this.array, index + 1, this.Count - index);
+
+            this.array[index] = item;
+            this.Count++;
+        }
+
+        public void InsertRange(int index, IEnumerable<T> items)
+        {
+            if (index < 0 || index > this.Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            int itemsCount = items.Count();
+
+            this.EnsureCapacity(this.Count + itemsCount);
+
+            Array.Copy(this.array, index, this.array, index + itemsCount, this.Count - index);
+            foreach (var item in items)
+            {
+                this.array[index++] = item;
+            }
+
+            this.Count += itemsCount;
+        }
+
+        public List<T> GetRange(int index, int length)
+        {
+            if (index < 0 || index > this.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            if (length < 0)
+            {
+                throw new ArgumentException();
+            }
+
+            if (index + length > this.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            List<T> result = new List<T>(length);
+            Array.Copy(this.array, index, result.array, 0, length);
+            result.Count = length;
+
+            return result;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index > this.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            Array.Copy(this.array, index + 1, this.array, index, this.Count - index - 1);
+
+            this.array[this.Count] = default(T);
+            this.Count--;
+        }
+
+        public void RemoveRange(int index, int length)
+        {
+            if (index < 0 || index > this.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            if (length < 0)
+            {
+                throw new ArgumentException();
+            }
+
+            if (index + length > this.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            Array.Copy(this.array, index + length, this.array, index, this.Count - index - length);
+
+            for (int i = this.Count - 1; i >= this.Count - length; i--)
+            {
+                this.array[i] = default(T);
+            }
+            this.Count -= length;
+        }
+
+        public int IndexOf(T item)
+        {
+            return Array.IndexOf(this.array, item);
+        }
+
+        public bool Remove(T item)
+        {
+            int index = this.IndexOf(item);
+            if (index >= 0)
+            {
+                this.RemoveAt(index);
+                return true;
+            }
+            return false;
+        }
+
+        public void RemoveAll(Predicate<T> predicate)
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (predicate(this.array[i]))
+                {
+                    this.RemoveAt(i--);
+                }
+            }
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                yield return this.array[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+        private void EnsureCapacity()
+        {
+            if (this.Count == this.Capacity)
+            {
+                Array.Resize(ref this.array, this.Capacity == 0 ? 4 : this.Capacity * 2);
+            }
+        }
+
+        private void EnsureCapacity(int targetCapacity)
+        {
+            if (targetCapacity > this.Capacity)
+            {
+                int doubleCapacity = this.Capacity == 0 ? 4 : this.Capacity * 2;
+                Array.Resize(ref this.array, targetCapacity > doubleCapacity ? targetCapacity : doubleCapacity);
+            }
+        }
+    }
+}
